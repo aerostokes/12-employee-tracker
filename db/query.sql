f03081e0e0cb1dc17c79515c4c1c4022497a3121
Rhonda Stokes <rhonda@aerostokes.com>
2023-05-02 21:21:50 -0700

    add js functionality to add to db

--- db/query.sql
+++ db/query.sql
@@ -1,11 +1,11 @@
 -- Active: 1682615646290@@127.0.0.1@3306@company_db
 USE company_db; 
 SELECT * FROM departments; 
-SELECT roles.id, roles.title, roles.salary, departments.name AS department 
+SELECT roles.id, roles.title, departments.name AS department, roles.salary 
 FROM roles LEFT JOIN departments ON department_id = departments.id; 
 SELECT employees.id, employees.first_name, employees.last_name, 
-employee_role.title AS role, employee_role.salary, departments.name AS department, 
-CONCAT(managers.last_name, ", " , managers.first_name, " (", manager_role.title, ")") AS manager
+employee_role.title AS role, departments.name AS department, employee_role.salary, 
+CONCAT(managers.last_name, ', ' , managers.first_name, ' (', manager_role.title, ')') AS manager
 FROM employees LEFT JOIN roles AS employee_role
 ON employees.role_id = employee_role.id
 LEFT JOIN departments
@@ -16,13 +16,18 @@ LEFT JOIN roles AS manager_role
 ON managers.role_id = manager_role.id;
 
 
+INSERT INTO departments (name) VALUES ('Test Department');
 
-INSERT INTO departments (name) VALUES ("Test Department");
-INSERT INTO roles (title, salary, department_id) VALUES ("Test Role", 0, 1);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Test", "Empolyee", 1, 3);
-UPDATE employees SET role_id = 2 WHERE id = 7;
+INSERT INTO roles (title, salary, department_id) VALUES ('Test Role', 0, 1);
+
+SELECT roles.id, CONCAT(departments.name, '  -  ', roles.title) AS name FROM roles LEFT JOIN departments ON department_id = departments.id ORDER BY department_id; 
+SELECT employees.id, CONCAT(last_name, ', ', first_name, ' (', title, ')') AS name FROM employees LEFT JOIN roles ON role_id = roles.id ORDER BY last_name;
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Test', 'Empolyee', 1, 3);
+UPDATE employees SET role_id = 2, manager_id = 4 WHERE id = 7;
 
 DELETE FROM departments WHERE id = 5;
 DELETE FROM roles WHERE id = 8;
 
 DELETE FROM employees WHERE id = 8;
+
+
--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function init() {
 };
 
 function chooseAction () {
-    const actionArr = ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee's Role", "Exit"];
+    const actionArr = ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee's Role/Manager", "Exit"];
     inquirer.prompt({
         type: "list",
         name: "nextAction",
@@ -82,7 +82,7 @@ function viewDepartments() {
 };
 
 function viewRoles() {
-    db.query("SELECT roles.id, roles.title, roles.salary, departments.name AS department FROM roles LEFT JOIN departments ON department_id = departments.id; ", (err, results) => {
+    db.query("SELECT roles.id, roles.title, departments.name AS department, roles.salary FROM roles LEFT JOIN departments ON department_id = departments.id; ", (err, results) => {
         if (err) { console.log("\x1b[31m", "Error reading database") };
         console.table("\x1b[36m", results);
         chooseAction();
@@ -91,7 +91,7 @@ function viewRoles() {
 
 function viewEmployees() {
     db.query(`SELECT employees.id, employees.first_name, employees.last_name, 
-    employee_role.title AS role, employee_role.salary, departments.name AS department, 
+    employee_role.title AS role, departments.name AS department, employee_role.salary, 
     CONCAT(managers.last_name, ", " , managers.first_name, " (", manager_role.title, ")") AS manager
     FROM employees LEFT JOIN roles AS employee_role
     ON employees.role_id = employee_role.id
@@ -111,7 +111,7 @@ function addDepartment() {
     inquirer.prompt({
         type: "input",
         name: "name",
-        message: "Department Name: ",
+        message: "Enter Department Name: ",
     }).then(departmentObj => {
         db.query("INSERT INTO departments (name) VALUES (?);", departmentObj.name, (err, results) => {
             if (err) { console.log("\x1b[31m", "Error writing to database") };
@@ -125,15 +125,16 @@ async function addRole() {
     try {
         const departmentsPromise = await db.promise().query("SELECT * FROM departments");
         const departmentsArr = departmentsPromise[0];
+        console.log(departmentsArr);
         const responsesObj = await inquirer.prompt([
             {
                 type: "input",
                 name: "title",
-                message: "Role Title: ",
+                message: "Enter Role Title: ",
             }, {
                 type:"input",
                 name: "salary",
-                message: "Salary: ",
+                message: "Enter Salary: ",
                 validate:  input => { 
                     const checkExp = new RegExp(/^[0-9]+$/);
                     if (checkExp.test(input)) { return true; 
@@ -143,7 +144,7 @@ async function addRole() {
             },{
                 type: "list",
                 name: "departmentName",
-                message: "Department: ",
+                message: "Select Department: ",
                 choices: departmentsArr,
             },
         ]);
@@ -154,17 +155,98 @@ async function addRole() {
     } catch {
         console.log("\x1b[31m", "Error writing to database");
         chooseAction();
-    }
+    };
 };
 
-function addEmployee() {
-    console.log("add an employee");
-
+async function addEmployee() {
+    try {
+        const rolesPromise = await db.promise().query("SELECT roles.id, CONCAT(departments.name, ' - ', roles.title) AS name FROM roles LEFT JOIN departments ON department_id = departments.id ORDER BY department_id;");
+        const rolesArr = rolesPromise[0];
+        const managersPromise = await db.promise().query("SELECT employees.id, CONCAT(last_name, ', ', first_name, ' (', title, ')') AS name FROM employees LEFT JOIN roles ON role_id = roles.id ORDER BY last_name;");
+        const managersArr = managersPromise[0];
+        const responsesObj = await inquirer.prompt([
+            {
+                type: "input",
+                name: "first_name",
+                message: "Enter First Name: ",
+            }, {
+                type: "input",
+                name: "last_name",
+                message: "Enter Last Name: ",
+            },{
+                type: "list",
+                name: "roleName",
+                message: "Select Role: ",
+                choices: rolesArr,
+            },{
+                type: "list",
+                name: "managerName",
+                message: "Select Manager : ",
+                choices: managersArr,
+            },
+        ]);
+        const roleId = rolesArr.filter(roleObj => roleObj.name == responsesObj.roleName)[0].id;
+        const managerId = managersArr.filter(managerObj => managerObj.name == responsesObj.managerName)[0].id;
+        await db.promise().query("INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);", [responsesObj.first_name, responsesObj.last_name, roleId, managerId]);
+        console.log("\x1b[36m", `${responsesObj.first_name} ${responsesObj.last_name} successfully added to Employees:`);
+        viewEmployees();
+    } catch {
+        console.log("\x1b[31m", "Error writing to database");
+        chooseAction();
+    };
 };
 
-function updateEmployeeRole() {
-    console.log("update employee's role");
-
+async function updateEmployeeRole() {
+    try {
+        const employeesPromise = await db.promise().query("SELECT employees.id, CONCAT(last_name, ', ', first_name, ' (', title, ')') AS name FROM employees LEFT JOIN roles ON role_id = roles.id ORDER BY last_name;");
+        const employeesArr = employeesPromise[0];
+        const rolesPromise = await db.promise().query("SELECT roles.id, CONCAT(departments.name, ' - ', roles.title) AS name FROM roles LEFT JOIN departments ON department_id = departments.id ORDER BY department_id;");
+        const rolesArr = rolesPromise[0];
+        const managersPromise = await db.promise().query("SELECT employees.id, CONCAT(last_name, ', ', first_name, ' (', title, ')') AS name FROM employees LEFT JOIN roles ON role_id = roles.id ORDER BY last_name;");
+        const managersArr = managersPromise[0];
+        const responsesObj = await inquirer.prompt([
+            {
+                type: "list",
+                name: "employeeName",
+                message: "Select Employee to Update: ",
+                choices: employeesArr,
+            },{
+                type: "list",
+                name: "roleName",
+                message: "Select New Role : ",
+                choices: rolesArr,
+            },{
+                type: "list",
+                name: "managerName",
+                message: "Select New Manager : ",
+                choices: managersArr,
+            },
+        ]);
+        const employeeId = employeesArr.filter(employeeObj => employeeObj.name == responsesObj.employeeName)[0].id;
+        const roleId = rolesArr.filter(roleObj => roleObj.name == responsesObj.roleName)[0].id;
+        const managerId = managersArr.filter(managerObj => managerObj.name == responsesObj.managerName)[0].id;
+        await db.promise().query("UPDATE employees SET role_id = ?, manager_id = ? WHERE id = ?;", [roleId, managerId, employeeId]);
+        console.log("\x1b[36m", `Successfully updated:`);
+        db.query(`SELECT employees.id, employees.first_name, employees.last_name, 
+        employee_role.title AS role, departments.name AS department, employee_role.salary, 
+        CONCAT(managers.last_name, ", " , managers.first_name, " (", manager_role.title, ")") AS manager
+        FROM employees LEFT JOIN roles AS employee_role
+        ON employees.role_id = employee_role.id
+        LEFT JOIN departments
+        ON employee_role.department_id = departments.id
+        LEFT JOIN employees AS managers
+        ON employees.manager_id = managers.id
+        LEFT JOIN roles AS manager_role
+        ON managers.role_id = manager_role.id
+        WHERE employees.id = ?;`, employeeId, (err, results) => {
+            // if (err) { console.log("\x1b[31m", "Error reading database") };
+            console.table("\x1b[36m", results);
+            chooseAction();
+        });
+    } catch {
+        console.log("\x1b[31m", "Error writing to database");
+        chooseAction();
+    };
 };
 
 function outro () {
